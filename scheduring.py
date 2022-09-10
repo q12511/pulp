c3645f6166beeec474a6846c7dd1d4241e3cc8ea
q12511 <snowsnow201596@gmail.com>
2022-09-10 21:42:03 +0900

    update
    yes 2 holiday
    no 3 holiday

--- scheduring.py
+++ scheduring.py
@@ -1,7 +1,7 @@
 #%%
 import pulp
 import pandas as pd
-
+import pprint
 # number of days
 ND = 30
 
@@ -9,7 +9,7 @@ w_df = pd.read_csv('worker.csv')
 s_df = pd.read_csv('shift.csv')
 
 # model instance
-prob = pulp.LpProblem('ShiftScheduring', pulp.LpMaximize)
+prob = pulp.LpProblem('ShiftScheduring', pulp.LpMinimize)
 
 # worker_list
 W = w_df['worker_id'].tolist()
@@ -29,75 +29,48 @@ WDS = [(w,d,s) for w in W for d in D for s in S]
 x = pulp.LpVariable.dicts('x', WDS, cat='Binary')
 
 # 連休あるかないか
-y = pulp.LpVariable.dicts('y', [(w,d) for w in W for d in D], cat='Binary')
+y = pulp.LpVariable.dicts('y', [(w,d) for w in W for d in D[:-1]], cat='Binary')
+#y = pulp.LpVariable.dicts('y', [(w) for w in W], cat='Binary')
+
+z = pulp.LpVariable.dicts('z', [(w,d) for w in W for d in D[:-2]], cat='Binary')
+
+#prob += -2 * pulp.lpSum(y[w,d] for w in W for d in D[:-1]) + 8 * pulp.lpSum(z[w,d] for w in W for d in D[:-2])
 
 # (0) 各作業員はそれぞれの日に一つのシフトにつく
 for w in W:
   for d in D:
     prob += pulp.lpSum([x[w,d,s] for s in S]) == 1
 
+# 2連休
+for w in W:
+  for d in D[:-1]:
+    prob += x[w,d,S[7]] + x[w,d+1,S[7]] -1 <= y[w,d]
+    prob += x[w,d,S[7]] + x[w,d+1,S[7]] >= 2 * y[w,d]
+# ３連休
+for w in W:
+  for d in D[:-2]:
+    prob += x[w,d,S[7]] + x[w,d+1,S[7]] + x[w,d+2,S[7]] - 2 <= z[w,d]
+    prob += x[w,d,S[7]] + x[w,d+1,S[7]] + x[w,d+2,S[7]] >= 3 * z[w,d]
 
-# (1) 各日に一人の作りを割り当てる
-#作り集合
+for w in W:
+  prob += pulp.lpSum(y[w,d] for d in D[:-1]) >= 1
+  prob += pulp.lpSum(z[w,d] for d in D[:-2]) == 0
 W_cook = [row.worker_id for row in w_df.itertuples() if row.G_flag == 1]
 W_Ncook = [row.worker_id for row in w_df.itertuples() if row.G_flag == 0]
 #print(W_cook)
 for d in D:
   prob += pulp.lpSum([x[w,d,S[5]] + x[w,d,S[6]] for w in W_cook]) == 1
   prob += pulp.lpSum([x[w,d,S[5]] + x[w,d,S[6]] for w in W_Ncook]) == 0
-
-# (2) 朝3人　昼３人　夜３人
-S_M = [row.shift_sign for row in s_df.itertuples() if row.morning == 1]
-S_D = [row.shift_sign for row in s_df.itertuples() if row.daytime == 1]
-S_N = [row.shift_sign for row in s_df.itertuples() if row.night == 1]
-#print(S_M)
-#print(S_D)
-#print(S_N)
-
-# (2).1 朝3人　昼３人　夜2人
 for d in D:
   prob += pulp.lpSum([x[w,d,S[0]] + x[w,d,S[1]] for w in W]) == 3
   prob += pulp.lpSum([x[w,d,S[1]] + x[w,d,S[2]] + x[w,d,S[4]] for w in W]) == 3
   prob += pulp.lpSum([x[w,d,S[3]] + x[w,d,S[4]] + x[w,d,S[6]] for w in W]) == 2
 
-# (3) 5連続勤務　禁止
-for w in W:
-  for d in D[4:]:
-    prob += pulp.lpSum([x[w,d - h,s] for h in range(4 + 1) for s in S[:7]]) <= 4
-
-# (4) B は一人
-for d in D:
-  prob += pulp.lpSum([x[w,d,S[4]] for w in W]) == 1
-
-# (5) G　２連続　禁止
-for w in W_cook:
-  for d in D[2:]:
-    prob += pulp.lpSum([x[w,d - h,S[5]] + x[w,d - h,S[6]] for h in range(2 + 1)]) <= 1
-
-# (6) GB は一回まで
-for w in W_cook:
-  prob += pulp.lpSum([x[w,d,S[6]] for d in D]) <= 1
-'''
-# (7) 必ず１回は２連休
-for w in W:
-  for d in D[:-2]:
-  #prob += pulp.lpSum([y[w,d]]) >= 0
-    prob += pulp.lpDot(x[w,d,S[7]],x[w,d+1,S[7]]) >= 1
-'''
 #　求解
 status = prob.solve()
 print(('Status:', pulp.LpStatus[status]))
 
-'''
-for i in range(1, ND):
-  print(i, end='                           ')
-print()
-for j in range(6):
-  for i in range(1, ND+1):
-    print(i, end='   ')
-print()
-'''
-
+# 表示
 print('          ', end='')
 for d in D:
   if d < 10:
@@ -133,4 +106,18 @@ for w in W:
           pass
       #print(x[w,d,s].value(), end=' ')
   print()
-
+'''
+for d in D:
+  if d < 10:
+    print(d, end='  ')
+  else:
+    print(d, end=' ')
+print()
+for w in W:
+  for d in D:
+    if y[w,d].value() == 1:
+      print('1', end='  ')
+    else:
+      print('0', end='  ')
+  print()
+'''
